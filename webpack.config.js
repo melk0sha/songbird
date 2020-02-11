3c1c1e4f1b9c3f8b4f93e0f666af7d2a4692b30a
unknown <din.ivanova13@gmail.com>
2020-02-11 11:27:27 +0300

    feat: add options for projects' needs

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Songbird",
+      template: "./src/index.html",
+      icon: "./src/assets/favicon.png"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "src/assets/favicon.png"),
+        to: path.resolve(__dirname, "dist")
+      }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name][hash].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
